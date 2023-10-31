aa2bb7389e13d7a294efb3d1f28d8ed19be76eca
JeremyXU <85468234+Jeremy1189@users.noreply.github.com>
2023-10-31 18:47:31 +0800

    Delete Avg_sub_Fe0.5Ni0.5 directory

--- Avg_sub_Fe0.5Ni0.5/DE_func/normalization.m
+++
@@ -1,9 +0,0 @@
-function Norm_X = normalization(X, var)
-
-L = size(X, 1);
-
-Lower = var(1, :);
-
-Upper = var(2, :);
-
-Norm_X = (X - repmat(Lower, L, 1)) ./ repmat(Upper - Lower, L, 1);
--- Avg_sub_Fe0.5Ni0.5/function/lammps_energy_displacement.m
+++
@@ -1,7 +0,0 @@
-function [energy,displacement] = lammps_energy_displacement(lammps_work_path,inputArg2)
-%LAMMPS_ENERGY_DISPLACEMENT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
